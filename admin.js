bf45b29d8c57a930c05f5bee722a027e72159bc2
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-07-10 21:31:00 +0200

    request param interceptor

--- admin.js
+++ admin.js
@@ -1,4 +1,5 @@
 var myApp = angular.module('myApp', ['ng-admin']);
+
 myApp.config(['NgAdminConfigurationProvider', function (nga) {
     // create an admin application
     var admin = nga.application('My First Admin')
@@ -14,7 +15,7 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
     ]);
     // add the user entity to the admin application
     admin.addEntity(user);
-    
+
     var post = nga.entity('posts');
     post.listView().fields([
         nga.field('id'),
@@ -25,4 +26,33 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
 
     // attach the admin application to the DOM and execute it
     nga.configure(admin);
+}]);
+
+myApp.config(['RestangularProvider', function (RestangularProvider) {
+    RestangularProvider.addFullRequestInterceptor(function(element, operation, what, url, headers, params) {
+        if (operation == "getList") {
+            // custom pagination params
+            if (params._page) {
+                params._start = (params._page - 1) * params._perPage;
+                params._end = params._page * params._perPage;
+            }
+            delete params._page;
+            delete params._perPage;
+            // custom sort params
+            if (params._sortField) {
+                params._sort = params._sortField;
+                params._order = params._sortDir;
+                delete params._sortField;
+                delete params._sortDir;
+            }
+            // custom filters
+            if (params._filters) {
+                for (var filter in params._filters) {
+                    params[filter] = params._filters[filter];
+                }
+                delete params._filters;
+            }
+        }
+        return { params: params };
+    });
 }]);
